a91904327a37266bc1e07b08a9f9a4b1c92927dc
Lxliving <lxlivingxin@gmail.com>
2018-07-23 17:03:03 +0800

    增加了发布回答功能
    插入数据库表answer实现，未在前台显示

--- newtax/src/com/tax/vo/answer.java
+++ newtax/src/com/tax/vo/answer.java
@@ -3,11 +3,24 @@ package com.tax.vo;
 import java.util.*;
 
 public class answer {
-	private String ansID;
+	/* ansID,txt,date,good,kept,check */
+	private String userID;
+	private int ansID;
 	private String txt; 
 	private Date date;				// In mysql parameter date has its own date format
 	private int good;
 	private int kept;
+	private int checked;
+	
+	public answer(String userID,int ansID,String txt,Date date,int good,int kept,int checked ) {
+		this.setUserID(userID);
+		this.ansID=ansID;
+		this.txt=txt;
+		this.date=date;
+		this.good=good;
+		this.kept=kept;
+		this.checked=checked;
+	}
 	
 	public int getKeep() {
 		return kept;
@@ -41,11 +54,29 @@ public class answer {
 		this.txt = txt;
 	}
 
-	public String getAnsID() {
+
+	public int getCheck() {
+		return checked;
+	}
+
+	public void setCheck(int checked) {
+		this.checked = checked;
+	}
+
+	public int getAnsID() {
 		return ansID;
 	}
 
-	public void setAnsID(String ansID) {
+	public void setAnsID(int ansID) {
 		this.ansID = ansID;
 	}
+
+	public String getUserID() {
+		return userID;
+	}
+
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+
 }
